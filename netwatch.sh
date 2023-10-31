d03e101c778073921c0a1d0a672444873096a2bc
chaseleif <chaseleif@icloud.com>
2023-10-31 01:13:55 -0500

    made fancy

--- netwatch.sh
+++ netwatch.sh
@@ -1,59 +1,79 @@
 #! /usr/bin/env bash
 
+secs2ts() {
+  secs=$((total%60))
+  total=$((total/60))
+  mins=$((total%60))
+  hours=$((total/60))
+  days=$((hours/24))
+  if [ $days -gt 0 ] ; then
+    hours=$((hours%24))
+  fi
+  echo -n "Elapsed time: "
+  docomma=0
+  if [ $days -gt 0 ] ; then
+    echo -n "$days day"
+    if [ $days -ne 1 ] ; then echo -n "s" ; fi
+    docomma=1
+  fi
+  if [ $hours -gt 0 ] ; then
+    if [ $docomma -ne 0 ] ; then echo -n ", " ; else docomma=1 ; fi
+    echo -n "$hours hour"
+    if [ $hours -ne 1 ] ; then echo -n "s" ; fi
+  fi
+  if [ $mins -gt 0 ] ; then
+    if [ $docomma -ne 0 ] ; then echo -n ", " ; else docomma=1 ; fi
+    echo -n "$mins minute"
+    if [ $mins -ne 1 ] ; then echo -n "s" ; fi
+  fi
+  if [[ $docomma -eq 0 || $secs -gt 0 ]] ; then
+    if [ $docomma -ne 0 ] ; then echo -n ", " ; fi
+    echo -n "$secs second"
+    if [ $secs -ne 1 ] ; then echo -n "s" ; fi
+  fi
+  echo ""
+}
+
+lastbreak=0
+printtime() {
+  total=$(date "+%s")
+  if [ $((total-lastbreak)) -lt 10 ] ; then
+    printf "aborted . . .\n"
+    exit
+  fi
+  if [ "$lastbreak" -gt 0 ] ; then
+    printf '\e[A\e[K'
+  fi
+  lastbreak="$total"
+  total=$((total-start))
+  secs2ts
+}
+stty -echoctl
+trap 'printtime' SIGINT
+
 start=$(date "+%s")
-date
 
 url="ifconfig.me"
 
-echo -n "waiting for network "
-i=0
+echo "###
+# Press ctrl-c to print the current elapsed time
+# Press ctrl-c twice within 10 seconds to quit
+###
+"
+
+date
+printf "\nWaiting for network . . ."
 while true ; do
-  i=$((i+1))
-  if [ $i -eq 15 ] ; then
-    i=-10
-    stdbuf -o0 echo "."
-  else
-    stdbuf -o0 echo -n ". "
-  fi
   addr=$(curl --no-fail --connect-timeout 20 "$url" 2> /dev/null)
   if [ "$addr" != "" ] ; then
-    echo "
-$addr"
+    echo "$addr"
     break
   fi
   sleep 1
 done
 
 total=$(($(date "+%s")-start))
+printf "Network up !\n\nTotal ~ "
+secs2ts
+printf "\n"
 date
-secs=$((total%60))
-total=$((total/60))
-mins=$((total%60))
-hours=$((total/60))
-days=$((hours/24))
-if [ $days -gt 0 ] ; then
-  hours=$((hours%24))
-fi
-echo -n "Elapsed time: "
-docomma=0
-if [ $days -gt 0 ] ; then
-  echo -n "$days day"
-  if [ $days -ne 1 ] ; then echo -n "s" ; fi
-  docomma=1
-fi
-if [ $hours -gt 0 ] ; then
-  if [ $docomma -ne 0 ] ; then echo -n ", " ; else docomma=1 ; fi
-  echo -n "$hours hour"
-  if [ $hours -ne 1 ] ; then echo -n "s" ; fi
-fi
-if [ $mins -gt 0 ] ; then
-  if [ $docomma -ne 0 ] ; then echo -n ", " ; else docomma=1 ; fi
-  echo -n "$mins minute"
-  if [ $mins -ne 1 ] ; then echo -n "s" ; fi
-fi
-if [[ $docomma -eq 0 || $secs -gt 0 ]] ; then
-  if [ $docomma -ne 0 ] ; then echo -n ", " ; fi
-  echo -n "$secs second"
-  if [ $secs -ne 1 ] ; then echo -n "s" ; fi
-fi
-echo ""
