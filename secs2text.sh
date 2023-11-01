3e6a1d1c24abb4562d5960efeb04a4c6c4862e7d
chaseleif <chaseleif@icloud.com>
2023-11-01 00:12:59 -0500

    add weeks

--- secs2text.sh
+++ secs2text.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/env bash
 
 str="$1"
-valstr=(second minute hour day)
+valstr=(second minute hour day week)
 vals=($((str%60)))  # secs
 str=$((str/60))
 vals+=($((str%60))) # mins
 str=$((str/60))
 vals+=($((str%24))) # hours
-vals+=($((str/24))) # days
+str=$((str/24))
+vals+=($((str%7)))  # days
+vals+=($((str/7)))  # weeks
 str=""
-for i in $(seq 3 -1 0) ; do
+for i in $(seq $((${#vals[@]}-1)) -1 0) ; do
   { [ "$i" -ne 0 ] || [ -n "$str" ]; } && [ "${vals[$i]}" -eq 0 ] && continue
   [ -n "$str" ] && str="${str}, "
   str="${str}${vals[$i]} ${valstr[$i]}"
