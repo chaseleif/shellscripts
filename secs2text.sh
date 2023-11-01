b4d1c71741657c8560657f5c4209b665c9ee04ec
chaseleif <chaseleif@icloud.com>
2023-10-31 21:16:37 -0500

    nicer secs2text

--- secs2text.sh
+++ secs2text.sh
@@ -6,26 +6,24 @@ total=$((total/60))
 mins=$((total%60))
 hours=$((total/60))
 days=$((hours/24))
-docomma=0
 if [ $days -gt 0 ] ; then
   hours=$((hours%24))
-  echo -n "$days day"
-  if [ $days -ne 1 ] ; then echo -n "s" ; fi
-  docomma=1
+  str="$days day"
+  if [ $days -ne 1 ] ; then str="${str}s" ; fi
 fi
 if [ $hours -gt 0 ] ; then
-  if [ $docomma -ne 0 ] ; then echo -n ", " ; else docomma=1 ; fi
-  echo -n "$hours hour"
-  if [ $hours -ne 1 ] ; then echo -n "s" ; fi
+  if [ -n "$str" ] ; then str="${str}, " ; fi
+  str="${str}${hours} hour"
+  if [ $hours -ne 1 ] ; then str="${str}s" ; fi
 fi
 if [ $mins -gt 0 ] ; then
-  if [ $docomma -ne 0 ] ; then echo -n ", " ; else docomma=1 ; fi
-  echo -n "$mins minute"
-  if [ $mins -ne 1 ] ; then echo -n "s" ; fi
+  if [ -n "$str" ] ; then str="${str}, " ; fi
+  str="${str}${mins} minute"
+  if [ $mins -ne 1 ] ; then str="${str}s" ; fi
 fi
-if [[ $docomma -eq 0 || $secs -gt 0 ]] ; then
-  if [ $docomma -ne 0 ] ; then echo -n ", " ; fi
-  echo -n "$secs second"
-  if [ $secs -ne 1 ] ; then echo -n "s" ; fi
+if [[ -z "$str" || $secs -gt 0 ]] ; then
+  if [ -n "$str" ] ; then str="${str}, " ; fi
+  str="${str}${secs} second"
+  if [ $secs -ne 1 ] ; then str="${str}s" ; fi
 fi
-echo ""
+echo "$str"
