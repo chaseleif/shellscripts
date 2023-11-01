06a5450d7cf93c4875285d2a787dfcd5d854f419
chaseleif <chaseleif@icloud.com>
2023-11-01 00:05:34 -0500

    no more

--- secs2text.sh
+++ secs2text.sh
@@ -1,26 +1,19 @@
 #! /usr/bin/env bash
 
-timestr="$1"
+str="$1"
 valstr=(second minute hour day)
-vals=($((timestr%60)))  # secs
-timestr=$((timestr/60))
-vals+=($((timestr%60))) # mins
-timestr=$((timestr/60))
-vals+=($((timestr%24))) # hours
-vals+=($((timestr/24))) # days
-timestr=""
-comma() { [ -n "$timestr" ] && timestr="${timestr}, " ; }
-plural() { [ "$1" -ne 1 ] && timestr="${timestr}s" ; }
-for i in $(seq 3 -1 1) ; do
-  [ "${vals[$i]}" -eq 0 ] && continue
-  comma
-  timestr="${timestr}${vals[$i]} ${valstr[$i]}"
-  plural "${vals[$i]}"
+vals=($((str%60)))  # secs
+str=$((str/60))
+vals+=($((str%60))) # mins
+str=$((str/60))
+vals+=($((str%24))) # hours
+vals+=($((str/24))) # days
+str=""
+for i in $(seq 3 -1 0) ; do
+  { [ "$i" -ne 0 ] || [ -n "$str" ]; } && [ "${vals[$i]}" -eq 0 ] && continue
+  [ -n "$str" ] && str="${str}, "
+  str="${str}${vals[$i]} ${valstr[$i]}"
+  [ "${vals[$i]}" -ne 1 ] && str="${str}s"
 done
-if [[ -z "$timestr" || "${vals[0]}" -gt 0 ]] ; then
-  comma
-  timestr="${timestr}${vals[0]} second"
-  plural "${vals[0]}"
-fi
-echo "$timestr"
+echo "$str"
 
