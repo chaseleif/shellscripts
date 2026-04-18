9e4f799930f3b45a54b8b58eb5a67b9ddb9fbe39
Chase Phelps <chaseleif@icloud.com>
2026-04-18 16:02:26 -0500

    rename var, use printf, add color, hide cursor, trap to restore cursor

--- choiceprompt.sh
+++ choiceprompt.sh
@@ -7,16 +7,28 @@ getchoice() {
   local key index i
   local -r var="$1"
   shift
-  local -r choices=("$@") maxindex="$((${#}-1))" clrlines="\e[${#}A"
+  local -r choices=("$@") maxindex="$((${#}-1))" mvcursorup="\e[${#}F"
   index=0
+  # hide cursor
+  printf "\e[?25l"
+  # don't leave the cursor hidden
+  trap 'printf "\e[?25h" ; trap - SIGINT ; kill -SIGINT $$' SIGINT
+  trap 'printf "\e[?25h" ; trap - SIGTERM ; kill -SIGTERM $$' SIGTERM
+  trap 'printf "\e[?25h" ; trap - SIGHUP ; kill -SIGHUP $$' SIGHUP
   while true ; do
+    # FG green; BG black
+    printf "\e[32;40m"
     for (( i=0; i<="$maxindex"; ++i )) ; do
       if [ "$i" -eq "$index" ] ; then
-        echo -e "~>\e[7m${choices[i]}\e[0m"
+        # 1m bold 22m reset bold
+        printf " \e[1m<%s>\e[22m\n" "${choices[i]}"
       else
-        echo "  ${choices[i]}"
+        # 0K del cursor to end of line
+        printf "  %s\e[0K\n" "${choices[i]}"
       fi
     done
+    # reset color
+    printf "\e[0m"
     while true ; do
       IFS= read -r -s -n1 key
       [ "$key" == "" ] && break
@@ -32,8 +44,11 @@ getchoice() {
       fi
     done
     [ "$key" == "" ] && break
-    echo -en "$clrlines"
+    # F move cursor to beginning of line # lines up
+    printf "%b" "$mvcursorup"
   done
+  # restore cursor
+  printf "\e[?25h"
   printf -v "$var" "%s" "${choices[$index]}"
 }
 choices=(
