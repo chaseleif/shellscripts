f552a3a768c7795268e0917257f59865259e7d1b
Chase Phelps <chaseleif@icloud.com>
2026-07-02 19:02:22 -0500

    add comments, move direction input check inside ctrl input

--- choiceprompt.sh
+++ choiceprompt.sh
@@ -4,6 +4,7 @@
 getchoice() {
   # the variable to set is the first argument
   local -r var="$1" ; shift
+  # ensure valid variable name
   if [[ ! $var =~ ^[A-Za-z_][A-Za-z0-9_]*$ ]] ; then
     printf "\"%s\" is not a valid variable name\n" "$var"
     return
@@ -13,8 +14,6 @@ getchoice() {
     printf "There are no choices to choose from to set %s\n" "$var"
     return
   fi
-  local key index i signal
-  local -r choices=("$@") maxindex="$((${#}-1))" mvcursorup="\e[${#}F"
   # save existing traps to restore them
   local -r intfn="$(trap -p SIGINT)"
   local -r termfn="$(trap -p SIGTERM)"
@@ -23,50 +22,62 @@ getchoice() {
   trap 'signal="SIGINT"' SIGINT
   trap 'signal="SIGTERM"' SIGTERM
   trap 'signal="SIGHUP"' SIGHUP
+  # constants
+  local -r choices=("$@") maxindex="$((${#}-1))" mvcursorup="\e[${#}F"
+  # variables
+  local key index i signal
   # hide cursor, FG green ; BG black
   printf "\e[?25l\e[32;40m"
-  # get a choice
+  # we start with index zero
   index=0
+  # loop to paint options, all options should fit in the screen
   while true ; do
+    # we start at the row/col of the first choice, print each choice
     for (( i=0; i<="$maxindex"; ++i )) ; do
       if [ "$i" -eq "$index" ] ; then
-        # 1m bold 22m reset bold
+        # highlight the active selection: 1m bold 22m reset bold
         printf " \e[1m<%s>\e[22m\n" "${choices[i]}"
       else
-        # 0K del cursor to end of line
+        # overwrite any prior highlight: 0K del cursor to end of line
         printf "  %s\e[0K\n" "${choices[i]}"
       fi
     done
+    # get a key, either to move the active row or choose the active row
     while [ -z "$signal" ] ; do
+      # read with a timeout, otherwise signals hang until some input received
       if IFS= read -r -s -n1 -t 0.2 key ; then
+        # `key` will be empty when enter is pressed
         [ -z "$key" ] && break
-      fi
-      if [ "$key" == $'\e' ] ; then
-        IFS= read -r -s -n2 -t0.05 key
-        # up
-        if [ "$key" == '[A' ] && [ "$index" -gt 0 ] ; then
-          ((--index))
-          break
-        # home
-        elif [ "$key" == '[H' ] && [ "$index" -gt 0 ] ; then
-          index=0
-          break
-        # down
-        elif [ "$key" == '[B' ] && [ "$index" -lt "$maxindex" ] ; then
-          ((++index))
-          break
-        # end
-        elif [ "$key" == '[F' ] && [ "$index" -lt "$maxindex" ] ; then
-          index="$maxindex"
-          break
+        # start of a command char
+        if [ "$key" == $'\e' ] ; then
+          # get the remainder of the input and handle directional keys
+          IFS= read -r -s -n2 -t0.05 key
+          # if the active selection changes we need to repaint the choices
+          # up
+          if [ "$key" == '[A' ] && [ "$index" -gt 0 ] ; then
+            ((--index))
+            break
+          # home
+          elif [ "$key" == '[H' ] && [ "$index" -gt 0 ] ; then
+            index=0
+            break
+          # down
+          elif [ "$key" == '[B' ] && [ "$index" -lt "$maxindex" ] ; then
+            ((++index))
+            break
+          # end
+          elif [ "$key" == '[F' ] && [ "$index" -lt "$maxindex" ] ; then
+            index="$maxindex"
+            break
+          fi
         fi
       fi
     done
+    # we received a signal or the key is empty (index was chosen)
     [[ -n "$signal" || -z "$key" ]] && break
     # nF move cursor to beginning of line n lines up
     printf "%b" "$mvcursorup"
   done
-  # restore state
   # remove our traps
   trap - SIGINT SIGTERM SIGHUP
   # restore the cursor and reset the color
@@ -75,16 +86,17 @@ getchoice() {
   [ -n "$intfn" ] && eval "$intfn"
   [ -n "$termfn" ] && eval "$termfn"
   [ -n "$hupfn" ] && eval "$hupfn"
-  # reraise signal
+  # reraise a signal if we trapped one
   if [ -n "$signal" ] ; then
     kill -"$signal" "$$"
-  # put choice into variable
+  # put the choice into the variable
   else
     printf -v "$var" "%s" "${choices[$index]}"
   fi
 }
 
 if false ; then
+  # sample usage
   choices=(
     "yes"
     "no"
